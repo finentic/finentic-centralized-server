943c3d954d8d18b9aff24febf8b0a33120123d21
luc.letuan <phantoinang2001@gmail.com>
2023-04-27 00:04:21 +0700

    Add account routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,22 @@
+const {
+    getAccount,
+    updateAvatar,
+    updateName,
+    updateBio,
+    updateExternalUrl,
+    create,
+} = require("../controllers/account.controller")
+
 const routers = (app) => {
     app.get('/', (_req, res) => res.status(200).json('Finentic centralized server is running.'))
+
+    app.get('/accounts/random', create)
+    app.get('/accounts/profile', getAccount)
+    app.post('/accounts/update-avatar', updateAvatar)
+    app.post('/accounts/update-name', updateName)
+    app.post('/accounts/update-bio', updateBio)
+    app.post('/accounts/update-external-url', updateExternalUrl)
+
 }
 
 module.exports = {
